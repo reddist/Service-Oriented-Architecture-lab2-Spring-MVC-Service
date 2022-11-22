83b379a340d054d40607cac64c0b6c17a18e7ffd
reddist <erddist@gmail.com>
2022-11-22 03:26:29 +0300

    initalize project

---
+++ src/main/java/lab/reddist/lab2springmvc/Lab2SpringMVCApplication.java
@@ -0,0 +1,13 @@
+package lab.reddist.lab2springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2SpringMVCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2SpringMVCApplication.class, args);
+	}
+
+}
